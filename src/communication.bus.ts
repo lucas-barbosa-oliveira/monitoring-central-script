dd49957c6224e6a5ecc80c25431e5637f097f628
lucas-barbosa-oliveira <lucas.barbosa@nutes.uepb.edu.br>
2019-06-06 09:10:16 -0300

    Starting Monitoring Central Script

---
+++ src/communication.bus.ts
@@ -0,0 +1,14 @@
+export class CommunicationBus {
+
+    public startConnection(): boolean {
+        return false;
+    }
+
+    public closeConnection(): boolean {
+        return false;
+    }
+
+    public sendMenssage(): boolean {
+        return false;
+    }
+}
---
+++ src/monitoring.central.ts
@@ -0,0 +1,42 @@
+export class MonitoringCentral {
+
+    public startSdnConfiguration(): boolean{
+        return false;
+    }
+
+    public startDicomServer(): boolean{
+        return false;
+    }
+
+    public startWireshark(): boolean{
+        return false;
+    }
+
+    public startOpenIce(): boolean{
+        return false;
+    }
+
+    public startUdpServer(): boolean{
+        return false;
+    }
+
+    public cleanSdnConfiguration(): boolean{
+        return false;
+    }
+
+    public closeDicomServer(): boolean{
+        return false;
+    }
+
+    public closetWireshark(): boolean{
+        return false;
+    }
+
+    public closeOpenIce(): boolean{
+        return false;
+    }
+
+    public closeUdpServer(): boolean{
+        return false;
+    }
+}
