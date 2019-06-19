3be175a4bcc6a6a62bc11f1e52364093bc7b98ac
lucas-barbosa-oliveira <lucas.barbosa@nutes.uepb.edu.br>
2019-06-19 10:05:03 -0300

    Add .gitignore

--- dist/utils/scripts.dicom.client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ScriptsDicomClient {
-}
-ScriptsDicomClient.SCRIPT_PATH = './scripts/';
-ScriptsDicomClient.NORMAL_SCENARIO = ScriptsDicomClient.SCRIPT_PATH + 'general/dicom_client/';
-ScriptsDicomClient.START_DICOM_CLIENT_SCRIPT = 'start_dicom_client.sh';
-ScriptsDicomClient.STOP_DICOM_CLIENT_SCRIPT = 'stop_dicom_client.sh';
-exports.ScriptsDicomClient = ScriptsDicomClient;
--- dist/utils/scripts.voip.client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ScriptsVoipClient {
-}
-ScriptsVoipClient.SCRIPT_PATH = './scripts/';
-ScriptsVoipClient.NORMAL_SCENARIO = ScriptsVoipClient.SCRIPT_PATH + 'general/voip_client/';
-ScriptsVoipClient.START_VOIP_CLIENT_SCRIPT = 'start_voip_client.sh';
-ScriptsVoipClient.STOP_VOIP_CLIENT_SCRIPT = 'stop_voip_client.sh';
-exports.ScriptsVoipClient = ScriptsVoipClient;
